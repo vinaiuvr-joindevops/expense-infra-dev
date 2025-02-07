502be68828fec90a63cd5528634dbfa96f75fb32
Vinai <uvr.vinaikumar@gmail.com>
2025-02-07 21:44:02 +0530

    Update parameters.tf

--- 10-sg/parameters.tf
+++ 10-sg/parameters.tf
@@ -3,3 +3,21 @@ resource "aws_ssm_parameter" "mysql_sg_id" {
   type  = "String"
   value = module.mysql_sg.sg_id
 }
+
+resource "aws_ssm_parameter" "backend_sg_id" {
+  name  = "/${var.project_name}/${var.environment}/backend_sg_id"
+  type  = "String"
+  value = module.backend_sg.sg_id
+}
+
+resource "aws_ssm_parameter" "frontend_sg_id" {
+  name  = "/${var.project_name}/${var.environment}/frontend_sg_id"
+  type  = "String"
+  value = module.frontend_sg.sg_id
+}
+
+resource "aws_ssm_parameter" "bastion_sg_id" {
+  name  = "/${var.project_name}/${var.environment}/bastion_sg_id"
+  type  = "String"
+  value = module.bastion_sg.sg_id
+}
