0a3709d469fbac314bb27aa42777e3e07ca86aa0
Vinai <uvr.vinaikumar@gmail.com>
2025-02-07 22:13:51 +0530

    Update bastionec2.tf

--- 20-bastion/bastionec2.tf
+++ 20-bastion/bastionec2.tf
@@ -3,12 +3,12 @@ resource "aws_instance" "bastion" {
   vpc_security_group_ids = [data.aws_ssm_parameter.bastion_sg_id ]
   instance_type          = "t3.micro"
   subnet_id = local.public_subnet_id
-  tags = merge{
+  tags = merge(
     var.common_tags,
     {
       Name = "${var.project_name}-${var.environment}-bastion
 
     }
 
-  }
+  )
 }
