ccd03c6d5488a5b77624e082562098df0b9e6a43
Vinai <uvr.vinaikumar@gmail.com>
2025-02-07 21:58:28 +0530

    Update data.tf

--- 10-sg/data.tf
+++ 10-sg/data.tf
@@ -1,3 +1,11 @@
 data "aws_ssm_parameter" "vpc_id" {
   name = "/${var.project_name}/${var.environment}/vpc_id"
 }
+
+data "aws_ssm_parameter" "bastion_sg_id" {
+  name = "/${var.project_name}/${var.environment}/bastion_sg_id"
+}
+
+data "aws_ssm_parameter" "public_sg_id" {
+  name = "/${var.project_name}/${var.environment}/public_sg_id"
+}
