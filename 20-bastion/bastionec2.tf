18e9c6193065ed61d2a0eb2d96b7992072fa8fa4
Vinai <uvr.vinaikumar@gmail.com>
2025-02-07 22:02:44 +0530

    Update bastionec2.tf

--- 20-bastion/bastionec2.tf
+++ 20-bastion/bastionec2.tf
@@ -1,9 +1,14 @@
 resource "aws_instance" "bastion" {
   ami                    = data.aws_ami.joindevops.id
-  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  vpc_security_group_ids = [data.aws_ssm_parameter.bastion_sg_id ]
   instance_type          = "t3.micro"
-  tags = {
-    Name    = "terraform-demo"
-    Purpose = "terraform-practice"
+  subnet_id = local.public_subnet_id
+  tags = merge{
+    var.common_tags,
+    {
+      Name = "${var.project_name}-${var.environment}-bastion
+
+    }
+
   }
 }
