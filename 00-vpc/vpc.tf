663858c581dd7101960596fdb8929670b635344f
Vinai <uvr.vinaikumar@gmail.com>
2025-02-07 21:09:48 +0530

    Create vpc.tf

---
+++ 00-vpc/vpc.tf
@@ -0,0 +1,13 @@
+module "vpc" {
+    #source = "../terraform-aws-vpc"
+    source = "git::https://github.com/vinaiuvr-joindevops/terraform-aws-vpc.git?ref=main"
+    project_name = var.project_name
+    environment = var.environment
+    vpc_cidr = var.vpc_cidr
+    common_tags = var.common_tags
+    vpc_tags = var.vpc_tags
+    public_subnet_cidrs = var.public_subnet_cidrs
+    private_subnet_cidrs = var.private_subnet_cidrs
+    database_subnet_cidrs = var.database_subnet_cidrs
+    is_peering_required = true
+}
