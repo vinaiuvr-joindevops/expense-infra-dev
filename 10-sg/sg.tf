6949d903011156f6ef9f568fd8427a775fcebd98
Vinai <uvr.vinaikumar@gmail.com>
2025-02-10 19:32:04 +0530

    Update sg.tf

--- 10-sg/sg.tf
+++ 10-sg/sg.tf
@@ -42,3 +42,14 @@ module "bastion_sg" {
     
     }
 
+module "app_alb_sg" {
+    source = "git::https://github.com/vinaiuvr-joindevops/terraform-aws-securitygroup.git?ref=main""
+    project_name = var.project_name
+    environment = var.environment
+    sg_name = "app_alb_sg"
+    sg_description = "created for backend_alb instances in expense dev"
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
+    common_tags = var.common_tags
+    
+    }
+
