8dc8d908ea5b43ae9043f5032e757163533768d2
Vinai <uvr.vinaikumar@gmail.com>
2025-02-10 19:31:18 +0530

    Update parameters.tf

--- 10-sg/parameters.tf
+++ 10-sg/parameters.tf
@@ -21,3 +21,10 @@ resource "aws_ssm_parameter" "bastion_sg_id" {
   type  = "String"
   value = module.bastion_sg.sg_id
 }
+
+
+resource "aws_ssm_parameter" "app_alb_sg_id" {
+  name  = "/${var.project_name}/${var.environment}/app_alb_sg_id"
+  type  = "String"
+  value = module.app_alb_sg.sg_id
+}
