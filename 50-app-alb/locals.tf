57976ba7ea7828b23276f5ed53e33cba4ff7674f
Vinai <uvr.vinaikumar@gmail.com>
2025-02-10 19:43:11 +0530

    Add files via upload

---
+++ 50-app-alb/locals.tf
@@ -0,0 +1,6 @@
+locals {
+    # stringlist to list 
+    private_isubnet_ids = join(",",data.aws_ssm_parameter.private_isubnet_ids.value)
+    app_alb_sg_id = data.aws_ssm_parameter.app_alb_sg.id.value
+}
+
